d6953ac05155d4da4dd1faee0a271a0b409a9821
ZhaZhaFon <zifeng_z@foxmail.com>
2023-01-11 21:37:00 +0800

    2023-01-10

--- fs_deploy_alpha-1.py
+++ fs_deploy_alpha-1.py
@@ -4,124 +4,47 @@
 ### TODO ###
 
 file_name = 'https://github.com/ZhaZhaFon/ScoringRanking/files/10374068/results_20221231.xlsx'
-#file_name = 'https://github.com/ZhaZhaFon/ScoringRanking/blob/master/results_20221231.csv?raw=true'
-top = 100
 
 ### TODO ###
 
-# 依赖
-
-import numpy as np
 import pandas as pd
-import os
-import time
 
 import streamlit as st
-
-from pyecharts.charts import *
-from pyecharts import options as opts
 
 # 标题
 
 st.set_page_config(layout="centered", page_icon="🎓", page_title="")
-st.title("基金量化打分 - 主动权益基金")
-st.write(f"呈现结果源于: [{file_name.split('/')[-1]}]({file_name}) （仅展示前{top}名）")
+st.title("基金量化评分 - 主动权益基金")
 
 # 数据
 
-ranking = pd.read_excel(file_name, index_col=0).reset_index().rename(columns={"index": "基金代码"}).iloc[:top, :]
+st.write("`#` 数据加载(约30s)...")
+ranking = pd.read_excel(file_name, index_col=0).reset_index().rename(columns={"index": "基金代码"})
 this_fund = ranking.iloc[0, :]
+st.write("`#` 加载完毕(约30s).")
 
-# 交互式可视化
+# 布局
 
-from st_aggrid import AgGrid, GridOptionsBuilder
-from st_aggrid.shared import GridUpdateMode
+left, right = st.columns(2)
+left.write("##### 【基金信息】")
+form = left.form("template_form")
 
-def aggrid_interactive_table(df: pd.DataFrame):
-    options = GridOptionsBuilder.from_dataframe(
-        df, enableRowGroup=True, enableValue=True, enablePivot=True
+right.write("##### 【量化评分】")
+
+# 左侧布局
+
+
+
+
+'''
+st.write(f"呈现结果源于: [{file_name.split('/')[-1]}]({file_name})")
+
+col1, col2 = st.columns(2)
+
+ans = col1.selectbox(
+        "123",
+        options=(1, 2, 3)
     )
-    options.configure_side_bar()
-    options.configure_selection("single")
-    selection = AgGrid(
-        df,
-        enable_enterprise_modules=True,
-        gridOptions=options.build(),
-        update_mode=GridUpdateMode.MODEL_CHANGED,
-        allow_unsafe_jscode=True,
-    )
-    return selection
+'''
 
-import streamlit_echarts
-
-def product_radar(this_fund):
-    this_scores = [[this_fund['分项合计-产品'].astype('float').round(2),
-                    this_fund['盈利得分-产品'].astype('float').round(2),
-                    this_fund['回撤控制得分-产品'].astype('float').round(2),
-                    this_fund['业绩稳定性得分-产品'].astype('float').round(2),
-                    this_fund['风险收益获取能力得分-产品'].astype('float').round(2),
-                    this_fund['规模得分-产品'].astype('float').round(2)]]
-    average_scores = [[(ranking['分项合计-产品'].sum()/len(ranking)).round(2),
-                    (ranking['盈利得分-产品'].sum()/len(ranking)).round(2),
-                    (ranking['回撤控制得分-产品'].sum()/len(ranking)).round(2),
-                    (ranking['业绩稳定性得分-产品'].sum()/len(ranking)).round(2),
-                    (ranking['风险收益获取能力得分-产品'].sum()/len(ranking)).round(2),
-                    (ranking['规模得分-产品'].sum()/len(ranking)).round(2)]]
-    radar = (
-        Radar()
-        .add_schema(
-            schema=[
-                opts.RadarIndicatorItem(name="基金产品得分", min_=ranking['分项合计-产品'].min(), max_=ranking['分项合计-产品'].max()),
-                opts.RadarIndicatorItem(name="盈利能力", min_=ranking['盈利得分-产品'].min(), max_=ranking['盈利得分-产品'].max()),
-                opts.RadarIndicatorItem(name="回撤控制", min_=ranking['回撤控制得分-产品'].min(), max_=ranking['回撤控制得分-产品'].max()),
-                opts.RadarIndicatorItem(name="业绩稳定性", min_=ranking['业绩稳定性得分-产品'].min(), max_=ranking['业绩稳定性得分-产品'].max()),
-                opts.RadarIndicatorItem(name="风险收益获取能力", min_=ranking['风险收益获取能力得分-产品'].min(), max_=ranking['风险收益获取能力得分-产品'].max()),
-                opts.RadarIndicatorItem(name="基金规模", min_=1, max_=5)
-            ],
-            
-        )
-        .add(series_name=f"{this_fund['基金简称']}（{this_fund['基金代码']}）", 
-            data=this_scores,
-            color="#CD0000",
-            )
-        .add(series_name="平均水平", 
-            data=average_scores,
-            color="#5CACEE",
-            )
-        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-        .set_global_opts(
-            legend_opts=opts.LegendOpts(selected_mode="multiple",
-                                        pos_bottom='bottom',
-                                        pos_left='left',),
-            title_opts=opts.TitleOpts(title=f"基金产品得分", 
-                                    pos_top='top',
-                                    title_textstyle_opts=opts.TextStyleOpts(font_family='KaiTi', font_size=20)),
-        )
-    )
-    return radar
-
-radar_product = product_radar(this_fund)
-streamlit_echarts.st_pyecharts(
-    radar_product
-)
-
-while True:
-
-    time.sleep(0.5)
-
-    ## 交互式雷达图 - 基金产品/经理/公司维度
-
-    radar_product = product_radar(this_fund)
-
-    ## 交互式表格 - 基金量化打分结果
-
-    selection = aggrid_interactive_table(df=ranking)
-
-    if selection:
-        st.write("You selected:")
-        st.json(selection["selected_rows"])
-        fund_code = selection["selected_rows"][0]['基金代码']
-        this_fund = ranking[ranking['基金代码']==fund_code]
-        print(this_fund)
-
-    
+# 右侧布局
