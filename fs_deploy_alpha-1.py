1369f91699991771d3e7c22d9c8a625905fe3278
ZhaZhaFon <zifeng_z@foxmail.com>
2023-01-11 21:51:05 +0800

    2023-01-10

--- fs_deploy_alpha-1.py
+++ fs_deploy_alpha-1.py
@@ -11,6 +11,10 @@ import pandas as pd
 
 import streamlit as st
 
+from pyecharts.charts import *
+from pyecharts import options as opts
+import streamlit_echarts
+
 # 标题
 
 st.set_page_config(layout="centered", page_icon="🎓", page_title="")
@@ -20,9 +24,60 @@ st.title("基金量化评分 - 主动权益基金")
 #'''
 st.write("`#` 数据加载(约5s)...")
 ranking = pd.read_excel(file_name, index_col=0).reset_index().rename(columns={"index": "基金代码"})
+product_tuple = tuple(ranking['基金代码'].tolist())
+manager_tuple = tuple(ranking['基金经理'].tolist())
+company_tuple = tuple(ranking['基金公司'].tolist())
 st.write("`#` 加载完毕.")
 #'''
 st.write(f"`#` 呈现结果源于: [{file_name.split('/')[-1]}]({file_name})")
+
+# 雷达图 - 基金产品
+
+def product_radar(this_fund):
+    this_scores = [[this_fund['分项合计-产品'].astype('float').round(2),
+                    this_fund['盈利得分-产品'].astype('float').round(2),
+                    this_fund['回撤控制得分-产品'].astype('float').round(2),
+                    this_fund['业绩稳定性得分-产品'].astype('float').round(2),
+                    this_fund['风险收益获取能力得分-产品'].astype('float').round(2),
+                    this_fund['规模得分-产品'].astype('float').round(2)]]
+    average_scores = [[(ranking['分项合计-产品'].sum()/len(ranking)).round(2),
+                    (ranking['盈利得分-产品'].sum()/len(ranking)).round(2),
+                    (ranking['回撤控制得分-产品'].sum()/len(ranking)).round(2),
+                    (ranking['业绩稳定性得分-产品'].sum()/len(ranking)).round(2),
+                    (ranking['风险收益获取能力得分-产品'].sum()/len(ranking)).round(2),
+                    (ranking['规模得分-产品'].sum()/len(ranking)).round(2)]]
+    radar = (
+        Radar()
+        .add_schema(
+            schema=[
+                opts.RadarIndicatorItem(name="基金产品得分", min_=ranking['分项合计-产品'].min(), max_=ranking['分项合计-产品'].max()),
+                opts.RadarIndicatorItem(name="盈利能力", min_=ranking['盈利得分-产品'].min(), max_=ranking['盈利得分-产品'].max()),
+                opts.RadarIndicatorItem(name="回撤控制", min_=ranking['回撤控制得分-产品'].min(), max_=ranking['回撤控制得分-产品'].max()),
+                opts.RadarIndicatorItem(name="业绩稳定性", min_=ranking['业绩稳定性得分-产品'].min(), max_=ranking['业绩稳定性得分-产品'].max()),
+                opts.RadarIndicatorItem(name="风险收益获取能力", min_=ranking['风险收益获取能力得分-产品'].min(), max_=ranking['风险收益获取能力得分-产品'].max()),
+                opts.RadarIndicatorItem(name="基金规模", min_=1, max_=5)
+            ],
+            
+        )
+        .add(series_name=f"{this_fund['基金简称']}（{this_fund['基金代码']}）", 
+            data=this_scores,
+            color="#CD0000",
+            )
+        .add(series_name="平均水平", 
+            data=average_scores,
+            color="#5CACEE",
+            )
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(selected_mode="multiple",
+                                        pos_bottom='bottom',
+                                        pos_left='left',),
+            title_opts=opts.TitleOpts(title=f"基金产品得分", 
+                                    pos_top='top',
+                                    title_textstyle_opts=opts.TextStyleOpts(font_family='KaiTi', font_size=20)),
+        )
+    )
+    return radar
 
 # 布局
 
@@ -32,10 +87,18 @@ left.write("##### 【基金信息】")
 # 左侧布局
 
 form = left.form("template_form")
-ans = left.selectbox(
+fundcode = form.selectbox(
     "基金产品",
-    options=tuple(ranking['基金代码'].tolist())
+    options=product_tuple,
 )
+
+if fundcode in product_tuple:
+    this_fund = ranking[ranking['基金代码']==fundcode]
+    radar_product = product_radar(this_fund)
+    streamlit_echarts.st_pyecharts(
+        radar_product
+    )
+
 # 右侧布局
 
 right.write("##### 【量化评分】")
