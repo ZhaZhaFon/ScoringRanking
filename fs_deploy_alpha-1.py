364843cefe555ae95efc5b53900581060583da63
ZhaZhaFon <zifeng_z@foxmail.com>
2023-01-11 22:26:12 +0800

    2023-01-10

--- fs_deploy_alpha-1.py
+++ fs_deploy_alpha-1.py
@@ -44,7 +44,7 @@ def product_radar(this_fund):
                     (ranking['业绩稳定性得分-产品'].sum()/len(ranking)).round(2),
                     (ranking['风险收益获取能力得分-产品'].sum()/len(ranking)).round(2),
                     (ranking['规模得分-产品'].sum()/len(ranking)).round(2)]]
-    radar = (
+    radar_product = (
         Radar()
         .add_schema(
             schema=[
@@ -75,43 +75,126 @@ def product_radar(this_fund):
                                     title_textstyle_opts=opts.TextStyleOpts(font_family='KaiTi', font_size=20)),
         )
     )
-    return radar
+    return radar_product
+
+# 雷达图 - 基金经理
+
+def manager_rader(this_fund):
+    this_scores = [[this_fund['分项合计-经理'].astype('float').round(2),
+                    this_fund['盈利得分-经理'].astype('float').round(2),
+                    this_fund['风控得分-经理'].astype('float').round(2),
+                    this_fund['规模得分-经理'].astype('float').round(2),
+                    this_fund['投资经验得分-经理'].astype('float').round(2)]]
+    average_scores = [[(ranking['分项合计-经理'].sum()/len(ranking)).round(2),
+                    (ranking['盈利得分-经理'].sum()/len(ranking)).round(2),
+                    (ranking['风控得分-经理'].sum()/len(ranking)).round(2),
+                    (ranking['规模得分-经理'].sum()/len(ranking)).round(2),
+                    (ranking['投资经验得分-经理'].sum()/len(ranking)).round(2)]]
+    radar_manager = (
+        Radar()
+        .add_schema(
+            schema=[
+                opts.RadarIndicatorItem(name="基金经理得分", min_=ranking['分项合计-经理'].min(), max_=ranking['分项合计-经理'].max()),
+                opts.RadarIndicatorItem(name="盈利能力", min_=ranking['盈利得分-经理'].min(), max_=ranking['盈利得分-经理'].max()),
+                opts.RadarIndicatorItem(name="风险控制", min_=ranking['风控得分-经理'].min(), max_=ranking['风控得分-经理'].max()),
+                opts.RadarIndicatorItem(name="管理规模", min_=1, max_=5),
+                opts.RadarIndicatorItem(name="投资经验", min_=1, max_=5)
+            ],
+            
+        )
+        .add(series_name=f"{this_fund['基金经理']}", 
+            data=this_scores,
+            color="#CD0000",
+            )
+        .add(series_name="平均水平", 
+            data=average_scores,
+            color="#5CACEE",
+            )
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(selected_mode="multiple",
+                                        pos_bottom='bottom'),
+            title_opts=opts.TitleOpts(title=f"基金经理得分", 
+                                    pos_left='center',
+                                    pos_top='top', 
+                                    title_textstyle_opts=opts.TextStyleOpts(font_family='KaiTi', font_size=20)),
+        )
+    )
+    return radar_manager
+
+# 雷达图 - 基金公司
+
+def company_rader(this_fund):
+    this_scores = [[this_fund['分项合计-公司'].astype('float').round(2),
+                    this_fund['短期业绩得分-公司'].astype('float').round(2),
+                    this_fund['长期业绩得分-公司'].astype('float').round(2)]]
+    average_scores = [[(ranking['分项合计-公司'].sum()/len(ranking)).round(2),
+                    (ranking['短期业绩得分-公司'].sum()/len(ranking)).round(2),
+                    (ranking['长期业绩得分-公司'].sum()/len(ranking)).round(2)]]
+    radar_company = (
+        Radar()
+        .add_schema(
+            schema=[
+                opts.RadarIndicatorItem(name="基金公司得分", min_=ranking['分项合计-公司'].min(), max_=ranking['分项合计-公司'].max()),
+                opts.RadarIndicatorItem(name="短期业绩水平", min_=ranking['短期业绩得分-公司'].min(), max_=ranking['短期业绩得分-公司'].max()),
+                opts.RadarIndicatorItem(name="长期业绩水平", min_=ranking['长期业绩得分-公司'].min(), max_=ranking['长期业绩得分-公司'].max())
+            ],
+            
+        )
+        .add(series_name=f"{this_fund['基金简称']}（{this_fund['基金代码']}）", 
+            data=this_scores,
+            color="#CD0000",
+            )
+        .add(series_name="平均水平", 
+            data=average_scores,
+            color="#5CACEE",
+            )
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(selected_mode="multiple",
+                                        pos_bottom='bottom'),
+            title_opts=opts.TitleOpts(title=f"基金公司得分", 
+                                    pos_left='center',
+                                    pos_top='top', 
+                                    title_textstyle_opts=opts.TextStyleOpts(font_family='KaiTi', font_size=20)),
+        )
+    )
+    return radar_compnay
 
 # 布局
 
 left, right = st.columns(2)
-left.write("##### 【基金池】")
 
 # 左侧布局
 
-form = left.form("template_form")
-fundcode = form.selectbox(
-    "主动权益基金",
+left.write("##### 【基金池】")
+form_left = left.form("template_form")
+fundcode = form_left.selectbox(
+    label="主动权益基金",
     options=product_tuple,
 )
-plot = form.radio(
-        "Set label visibility 👇",
-        ["1", "2", "3"],
-     )
-submit = form.form_submit_button("量化评分")
+
+# 右侧布局
+
+right.write("##### 【量化评分维度】")
+form_right = right.form("template_form")
+radar_type = form_right.radio(
+    label="基金产品/基金经理/基金公司",
+    options=["基金产品", "基金经理", "基金公司"],
+)
+submit = form_left.form_submit_button("量化评分")
 
 if submit:
     this_fund = ranking[ranking['基金代码']==fundcode].iloc[0, :]
     radar_product = product_radar(this_fund)
-    if plot == "1":
-        streamlit_echarts.st_pyecharts(
-            radar_product
-        )
-    if plot == "2":
-        streamlit_echarts.st_pyecharts(
-            radar_product
-        )
-    if plot == "3":
-        streamlit_echarts.st_pyecharts(
-            radar_product
-        )
+    if radar_type == "基金产品":
+        streamlit_echarts.st_pyecharts(radar_product)
+    if radar_type == "基金经理":
+        streamlit_echarts.st_pyecharts(radar_product)
+    if radar_type == "基金公司":
+        streamlit_echarts.st_pyecharts(radar_product)
 
-# 右侧布局
+# 备注
 
 st.write("- 主动权益基金: Wind一级投资类型下的普通股票型、偏股混合型、平衡混合型、灵活配置型基金")
 st.write("- 基金池: 仅考虑现任基金经理任职>2年、最新规模>2亿元、过去5期平均权益仓位不低于60%的初始基金(A份额)作为样本")
