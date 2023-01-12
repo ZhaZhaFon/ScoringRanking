abc9b749560cb1dbc0c191c94e633e175d6db39d
ZhaZhaFon <zifeng_z@foxmail.com>
2023-01-12 18:56:26 +0800

    fs_deploy_alpha.py

--- fs_deploy_alpha.py
+++ fs_deploy_alpha.py
@@ -196,8 +196,8 @@ fund_company = ranking[ranking['基金代码']==fundcode]['基金公司'].item()
 
 ## 产品得分
 score_product = ranking[ranking['基金代码']==fundcode]['分项合计-产品'].item()
-score_product_all = ranking['分项合计-产品'].tolist().sort(reverse=True)
 print(ranking['分项合计-产品'])
+score_product_all = ranking['分项合计-产品'].tolist().sort(reverse=True)
 score_product_rank = score_product_all.index(score_product) + 1
 score_product = round(score_product, 2)
 
