c915636119a9864ac5069f83c227c515e9c91786
ZhaZhaFon <zifeng_z@foxmail.com>
2023-01-12 18:36:46 +0800

    fs_deploy_alpha.py

--- fs_deploy_alpha.py
+++ fs_deploy_alpha.py
@@ -190,6 +190,7 @@ fundname = ans.split(' ')[1]
 fund_manager = ranking[ranking['基金代码']==fundcode]['基金经理'].item()
 fund_company = ranking[ranking['基金代码']==fundcode]['基金公司'].item()
 
+print(ranking[ranking['基金代码']==fundcode]['分项合计-产品'].item())
 score_product = ranking[ranking['基金代码']==fundcode]['分项合计-产品'].item().astype('float').round(2)
 score_manager = ranking[ranking['基金代码']==fundcode]['分项合计-经理'].item().astype('float').round(2)
 score_company = ranking[ranking['基金代码']==fundcode]['分项合计-公司'].item().astype('float').round(2)
